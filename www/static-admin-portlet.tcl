639cfd816763bd60d391bde1493896ee3bd0c7e0
Yonatan Feldman <yon@milliped.com>
2002-05-06 23:57:19 +0000

    removed anything pertaining to user's portals for a community, we don't support that anymore; renamed sql files

--- www/static-admin-portlet.tcl
+++ www/static-admin-portlet.tcl
@@ -21,23 +21,21 @@ ad_page_contract {
     @author Ben Adida (ben@openforce)    
     @cvs_id $Id$
 } -properties {
-    
 }
 
 array set config $cf	
-
 set instance_id $config(instance_id)
 
 set element_pretty_name [ad_parameter static_admin_portlet_element_pretty_name static-portlet "Custom Portlet"]
-set element_pretty_plural \
-        [ad_parameter static_admin_portlet_element_pretty_plural static-portlet "Custom Portlets"]
+set element_pretty_plural [ad_parameter static_admin_portlet_element_pretty_plural static-portlet "Custom Portlets"]
 
+db_multirow content select_content {
+    select content_id,
+           pretty_name
+    from static_portal_content
+    where instance_id = :instance_id
+}
 
-db_multirow content select_content "
-select content_id, pretty_name
-from static_portal_content
-where instance_id = :instance_id"
-
-set template_portal_id [dotlrn_community::get_portal_template_id [dotlrn_community::get_community_id]]
+set template_portal_id [dotlrn_community::get_portal_id]
 set applet_url "[dotlrn_applet::get_url]/[static_portlet::my_package_key]"
 set referer [ad_conn url]
