968e8bc8db93345027f21200f6a7202845419821
Hector Romojaro <hector.romojaro@gmail.com>
2020-08-21 14:43:09 +0000

    Add 'static_portlet::get_pretty_name' to the 'static_portlet_links_names' test

--- tcl/test/static-portlet-procs.tcl
+++ tcl/test/static-portlet-procs.tcl
@@ -9,14 +9,16 @@ ad_library {
 aa_register_case -procs {
         static_admin_portlet::link
         static_portlet::link
+        static_portlet::get_pretty_name
     } -cats {
         api
         production_safe
-    } static_portlet_links {
-        Test diverse link procs.
+    } static_portlet_links_names {
+        Test diverse link and name procs.
 } {
-    aa_equals "Static admin portlet link" "[static_admin_portlet::link]" ""
-    aa_equals "Static portlet link"       "[static_portlet::link]" ""
+    aa_equals "Static admin portlet link"   "[static_admin_portlet::link]" ""
+    aa_equals "Static portlet link"         "[static_portlet::link]" ""
+    aa_equals "Static portlet pretty name"  "[static_portlet::get_pretty_name]" ""
 }
 
 # Local variables:
