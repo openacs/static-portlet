30021ad80b49a9e1b47e91a8cfbc72bb7a70125a
Hector Romojaro <hector.romojaro@gmail.com>
2020-08-24 11:18:30 +0000

    Add test case 'static_portal_content__procs'

--- tcl/test/static-portlet-procs.tcl
+++ tcl/test/static-portlet-procs.tcl
@@ -21,6 +21,71 @@ aa_register_case -procs {
     aa_equals "Static portlet pretty name"  "[static_portlet::get_pretty_name]" ""
 }
 
+
+aa_register_case -procs {
+        static_portal_content::new
+        static_portal_content::get_content
+        static_portal_content::get_pretty_name
+        static_portal_content::get_package_id
+        static_portal_content::get_content_format
+        static_portal_content::delete
+    } -cats {
+        api
+    } static_portal_content__procs {
+        Test diverse static_portal_content procs.
+} {
+    aa_run_with_teardown -rollback -test_code {
+        #
+        # Create the new content item 'foo'.
+        #
+        # As this is running in a transaction, it should be cleaned up
+        # automatically.
+        #
+        set content "Just a test"
+        set content_package_id 0
+        set content_pretty_name "foo"
+        set content_format "text/html"
+        set content_id [static_portal_content::new \
+                            -package_id $content_package_id \
+                            -content $content \
+                            -pretty_name $content_pretty_name \
+                            -format $content_format]
+        #
+        # Check if the creation was successful
+        #
+        set exists_p [db_0or1row foo {
+            select * from static_portal_content where content_id=:content_id
+        }]
+        aa_true "Creation successful (content_id: $content_id)" "$exists_p"
+        if {$exists_p} {
+            #
+            # Check the content
+            #
+            aa_equals "Check content" "[static_portal_content::get_content -content_id $content_id]" "$content"
+            #
+            # Check the pretty name
+            #
+            aa_equals "Check pretty name" "[static_portal_content::get_pretty_name -content_id $content_id]" "$content_pretty_name"
+            #
+            # Check the package id
+            #
+            aa_equals "Check package id" "[static_portal_content::get_package_id -content_id $content_id]" "$content_package_id"
+            #
+            # Check the content format
+            #
+            aa_equals "Check content format" "[static_portal_content::get_content_format -content_id $content_id]" "$content_format"
+            #
+            # Delete
+            #
+            static_portal_content::delete -content_id $content_id
+            aa_false "Deletion successful" "[db_0or1row foo {select * from static_portal_content where content_id=:content_id}]"
+        } else {
+            aa_error "Creation failed"
+        }
+    }
+}
+
+
 # Local variables:
 #    mode: tcl
 #    tcl-indent-level: 4
